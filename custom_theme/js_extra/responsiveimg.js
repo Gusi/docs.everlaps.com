9f86695dfe49e16469573fcd277ddbfbf6576119
Gusi <sergiorg@gmail.com>
2016-01-14 17:16:56 +0100

    Minor fixes

--- custom_theme/js_extra/responsiveimg.js
+++ custom_theme/js_extra/responsiveimg.js
@@ -3,17 +3,20 @@
 var modal = document.getElementById("responsiveimg_modal");
 function ImgResponsive_Close()
 {
-	modal.style.display = "none";
+	if (modal)
+		modal.style.display = "none";
 }
 
 // Get the <span> element that closes the modal
 var span = document.getElementById("responsiveimg_close");
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function()  { ImgResponsive_Close(); }
+if (span)
+	span.onclick = function()  { ImgResponsive_Close(); }
 
 var modalImg = document.getElementById("responsiveimg_content")
-modalImg.onclick = function() { ImgResponsive_Close(); }
+if (modalImg)
+	modalImg.onclick = function() { ImgResponsive_Close(); }
 
 // Get all images and insert the clicked image inside the modal
 // Get the content of the image description and insert it inside the modal image caption
